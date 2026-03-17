e618c93e3efd21580395257bf9a96c1b4680590b
DrinoSan <s.becirbegovic@gmail.com>
2026-03-17 22:27:39 +0100

    Added Warning and colored output

--- Log.c
+++ Log.c
@@ -5,7 +5,19 @@
 
 void log_info( const char* fmt, const char* file, int line, ... )
 {
-   printf( "[INFO] " );
+   printf( "\033[36m[INFO]\033[0m  " );
+
+   va_list args;
+   va_start( args, line );
+   vprintf( fmt, args );
+   va_end( args );
+
+   printf( " | %s:%d\n", file, line );
+}
+
+void log_warn( const char* fmt, const char* file, int line, ... )
+{
+   printf( "\033[38;5;208m[WARN]\033[0m  " );
 
    va_list args;
    va_start( args, line );
--- Log.h
+++ Log.h
@@ -2,5 +2,7 @@
 
 // Macro to have my lines and file names included
 #define LOG_INFO( fmt, ... ) log_info( fmt, __FILE__, __LINE__, ##__VA_ARGS__ )
+#define LOG_WARN( fmt, ... ) log_warn( fmt, __FILE__, __LINE__, ##__VA_ARGS__ )
 
 void log_info( const char* fmt, const char* file, int line, ... );
+void log_warn( const char* fmt, const char* file, int line, ... );
