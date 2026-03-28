841d89c6742149fb7c0a53f419f85222d3846c9b
DrinoSan <s.becirbegovic@gmail.com>
2026-03-28 20:56:42 +0100

    Added print statement for debuging to HttpRequest

--- HttpRequest.c
+++ HttpRequest.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 
 
@@ -14,4 +15,33 @@ const char* http_request_find_header(const HttpRequest_t* request, const char* n
    }
 
    return NULL;
+}
+
+void http_request_showMe( const HttpRequest_t* request )
+{
+   printf( "=== HttpRequest ===\n" );
+   printf( "method:         %s\n", request->method );
+   printf( "method_int:     0x%08X\n", request->method_int );
+   printf( "path:           %s\n", request->path );
+   printf( "version:        %s\n", request->version );
+   printf( "version_int:    %d\n", request->version_int );
+   printf( "http_major:     %d\n", request->http_major );
+   printf( "http_minor:     %d\n", request->http_minor );
+   printf( "state:          %d\n", request->state );
+   printf( "complex_uri:    %d\n", request->complex_uri );
+   printf( "content_length: %d\n", request->content_length );
+   printf( "body:           %s\n", request->body ? request->body : "(null)" );
+   printf( "request_start:  %p\n", (void*)request->request_start );
+   printf( "request_end:    %p\n", (void*)request->request_end );
+   printf( "schema_start:   %p\n", (void*)request->schema_start );
+   printf( "schema_end:     %p\n", (void*)request->schema_end );
+   printf( "method_end:     %p\n", (void*)request->method_end );
+   printf( "uri_start:      %p\n", (void*)request->uri_start );
+   printf( "uri_end:        %p\n", (void*)request->uri_end );
+   printf( "header_count:   %d\n", request->header_count );
+   for( int32_t i = 0; i < request->header_count; i++ )
+   {
+      printf( "  [%d] %s: %s\n", i, request->headers[ i ].name, request->headers[ i ].value );
+   }
+   printf( "===================\n" );
 }
--- HttpRequest.h
+++ HttpRequest.h
@@ -4,23 +4,38 @@
 
 #include "HttpHeader.h"
 
-#define SAND_HTTP_UNKNOWN   0x00000001
-#define SAND_HTTP_GET       0x00000002
-#define SAND_HTTP_HEAD      0x00000004
-#define SAND_HTTP_POST      0x00000008
-#define SAND_HTTP_PUT       0x00000010
-#define SAND_HTTP_DELETE    0x00000020
-#define SAND_HTTP_MKCOL     0x00000040
-#define SAND_HTTP_COPY      0x00000080
-#define SAND_HTTP_MOVE      0x00000100
-#define SAND_HTTP_OPTIONS   0x00000200
-#define SAND_HTTP_PROPFIND  0x00000400
-#define SAND_HTTP_PROPPATCH 0x00000800
-#define SAND_HTTP_LOCK      0x00001000
-#define SAND_HTTP_UNLOCK    0x00002000
-#define SAND_HTTP_PATCH     0x00004000
-#define SAND_HTTP_TRACE     0x00008000
-#define SAND_HTTP_CONNECT   0x00010000
+// clang-format off
+// Using power of twos because that way only one single bit is set in the mask
+// which i need for bit operations
+//
+// Each HEX digit is 4 bit digits            POWER OF
+#define SAND_HTTP_UNKNOWN     0x00000001   // 0
+#define SAND_HTTP_GET         0x00000002   // 1
+#define SAND_HTTP_HEAD        0x00000004   // 2
+#define SAND_HTTP_POST        0x00000008   // 3
+#define SAND_HTTP_PUT         0x00000010   // 4
+#define SAND_HTTP_DELETE      0x00000020   // 5
+#define SAND_HTTP_MKCOL       0x00000040   // 6
+#define SAND_HTTP_COPY        0x00000080   // 7
+#define SAND_HTTP_MOVE        0x00000100   // 8
+#define SAND_HTTP_OPTIONS     0x00000200   // 9
+#define SAND_HTTP_PROPFIND    0x00000400   // 10
+#define SAND_HTTP_PROPPATCH   0x00000800   // 11
+#define SAND_HTTP_LOCK        0x00001000   // 12
+#define SAND_HTTP_UNLOCK      0x00002000   // 13
+#define SAND_HTTP_PATCH       0x00004000   // 14
+#define SAND_HTTP_TRACE       0x00008000   // 15
+#define SAND_HTTP_CONNECT     0x00010000   // 16
+#define SAND_HTTP_ALL_METHODS 0x0001FFFF   // If we double SAND_HTTP_CONNECT by 2 then subtract one we get the ALL_Methos
+// By Doubeling we shift the one bit to the left (Division would shift right) Then if we subtract one if fills all the bits after with one
+// clang-format on
+
+// sand_string_view
+typedef struct
+{
+   char* data;
+   size_t      size;
+} sand_string_view_t;
 
 typedef struct
 {
@@ -28,20 +43,30 @@ typedef struct
    int32_t      method_int;
    char         path[ 256 ];
    char         version[ 16 ];   // HTTP/1.1
+   int32_t      version_int;     // For easier checking
    HttpHeader_t headers[ MAX_HEADERS ];
    int32_t      header_count;
    int32_t      content_length;
    char*        body;   // pointer into the connection buffer (no copy needed)
 
-   char* request_start;
-   char* method_end;
-   char* uri_start;
-   char* uri_end;
+   int32_t state;
+   char*   request_start;
+   char*   request_end;
+   char*   schema_start;
+   char*   schema_end;
+   char*   method_end;
+   char*   uri_start;
+   char*   uri_end;
+   sand_string_view_t uri_view;
+
 
    int32_t http_major;
    int32_t http_minor;
    int32_t complex_uri;
+
 } HttpRequest_t;
 
 const char* http_request_find_header( const HttpRequest_t* request,
                                       const char*          name );
+
+void http_request_showMe( const HttpRequest_t* request );
